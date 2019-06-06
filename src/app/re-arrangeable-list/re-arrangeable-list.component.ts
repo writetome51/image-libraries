750044e3bd430b8ada9b81768b503fa66b2d837f
Steve Thompson <writetome51@gmail.com>
2019-06-06 17:19:07 -0600

    Added <script> tags in index.html that include jquery-ui and jquery

--- src/app/re-arrangeable-list/re-arrangeable-list.component.ts
+++ src/app/re-arrangeable-list/re-arrangeable-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { moveByIndex } from '@writetome51/array-move-by-index';
+import * as $ from 'jquery-ui';
 
 
 @Component({
@@ -11,10 +12,29 @@ export class ReArrangeableListComponent {
 
 
     @Input() items;
-
-
     list = ['coffee', 'beer', 'soda', 'water', 'tea', 'milk'];
     indexBeingDragged: number;
+
+    private $: any;
+
+
+    reArrange(){
+        let panelList = $('#draggablePanelList');
+
+        panelList.sortable({
+            // Only make the .panel-heading child elements support dragging.
+            // Omit this to make then entire <li>...</li> draggable.
+            handle: '.panel-heading',
+            update: function() {
+                $('.panel', panelList).each(function(index, elem) {
+                    var $listItem = $(elem),
+                        newIndex = $listItem.index();
+
+                    // Persist the new indices.
+                });
+            }
+        });
+    }
 
 
     reorder(newIndex) {
