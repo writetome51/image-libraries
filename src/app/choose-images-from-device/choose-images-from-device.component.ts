a34ed228178b7be7149d26723fc1942a9d60083a
Steve Thompson <writetome51@gmail.com>
2019-07-09 17:30:49 -0600

    Created ChooseImagesFromDeviceComponent

---
+++ src/app/choose-images-from-device/choose-images-from-device.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'choose-images-from-device',
+  templateUrl: './choose-images-from-device.component.html'
+})
+export class ChooseImagesFromDeviceComponent {
+
+  constructor() { }
+
+
+
+}
