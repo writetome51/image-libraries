426a62a17ce7370d9c2245d6b41da83d808ca2be
Steve Thompson <writetome51@gmail.com>
2019-07-09 17:33:15 -0600

    Created EnterImageURLComponent

---
+++ src/app/enter-image-url/enter-image-url.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'enter-image-url',
+  templateUrl: './enter-image-url.component.html'
+})
+export class EnterImageURLComponent {
+
+  constructor() { }
+
+
+}
