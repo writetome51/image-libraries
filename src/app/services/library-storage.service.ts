57fa0b6e598849bef7974689dd692e9bb2e00c13
Steve Thompson <writetome51@gmail.com>
2019-09-07 17:12:58 -0600

    Minor refactoring done to LibraryStorageService

--- src/app/services/library-storage.service.ts
+++ src/app/services/library-storage.service.ts
@@ -1,8 +1,8 @@
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-import { CurrentUserService } from './current-user.service';
-import { LibrariesRestAPIService } from './libraries-rest-api.service';
 import { CurrentLibraryService } from './current-library.service';
+import { Injectable } from '@angular/core';
+import { LibrariesRestAPIService } from './libraries-rest-api.service';
+import { Observable } from 'rxjs';
+import { SessionIDLocalStorageService } from './session-id-local-storage.service';
 
 
 @Injectable({
@@ -13,26 +13,26 @@ export class LibraryStorageService {
 
 	constructor(
 		private __librariesRestApi: LibrariesRestAPIService,
-		private __currentUser: CurrentUserService,
-		private __currentLibrary: CurrentLibraryService
+		private __currentLibrary: CurrentLibraryService,
+		private __sessionIDLocalStorage: SessionIDLocalStorageService
 	) {
 	}
 
 
 	getLibrary(): Observable<any> {
 		return this.__librariesRestApi.getLibrary({
-			email: this.__currentUser.email,
-			password: this.__currentUser.password,
+			sessionID: this.__sessionIDLocalStorage.get(),
 			name: this.__currentLibrary.name
 		});
 	}
 
 
-	updateLibraries(): Observable<any> {
-		return this.__librariesRestApi.update({
-			email: this.__currentUser.email,
-			password: this.__currentUser.password,
-			newValue: this.__currentUser.activeLibrary
+	updateLibrary(): Observable<any> {
+		return this.__librariesRestApi.updateLibrary({
+			sessionID: this.__sessionIDLocalStorage.get(),
+			name: this.__currentLibrary.name,
+			// The properties in 'changes' can contain dot-notation
+			changes: {}
 		});
 	}
 
