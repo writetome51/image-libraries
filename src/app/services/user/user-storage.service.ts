41d694626ae4dbf6f3233c2faf67a3d840a4242c
Steve Thompson <writetome51@gmail.com>
2019-09-16 17:26:12 -0600

    Refactored UserStorageService to get rid of redundant code

--- src/app/services/user/user-storage.service.ts
+++ src/app/services/user/user-storage.service.ts
@@ -19,84 +19,66 @@ export class UserStorageService {
 
 
 	async create(): Promise<any> {
-		return new Promise((returnData) => {
-
-			let observable = this.__userRestApi.create({
+		return await this.__getSubscriptionData(
+			this.__userRestApi.create({
 				email: this.__currentUser.email,
 				password: this.__currentUser.password,
 				securityQuestion: this.__currentUser.securityQuestion
-			});
-
-			let subscription = observable.subscribe((result) => {
-				returnData(result);
-				subscription.unsubscribe();
-			});
-		});
+			})
+		);
 	}
 
 
 	async delete(): Promise<any> {
-		return new Promise((returnData) => {
-
-			let observable = this.__userRestApi.delete({
+		return await this.__getSubscriptionData(
+			this.__userRestApi.delete({
 				email: this.__currentUser.email,
 				password: this.__currentUser.password,
 				sessionID: this.__sessionIDLocalStorage.get()
-			});
-
-			let subscription = observable.subscribe((result) => {
-				returnData(result);
-				subscription.unsubscribe();
-			});
-		});
+			})
+		);
 	}
 
 
 	async get(): Promise<any> {
-		return new Promise((returnData) => {
-			let observable = this.__userRestApi.get({sessionID: this.__sessionIDLocalStorage.get()});
-
-			let subscription = observable.subscribe((result) => {
-				returnData(result);
-				subscription.unsubscribe();
-			});
-		});
+		return await this.__getSubscriptionData(
+			this.__userRestApi.get({sessionID: this.__sessionIDLocalStorage.get()})
+		);
 	}
 
 
 	async updatePassword(): Promise<any> {
-		return new Promise((returnData) => {
-
-			let observable = this.__userRestApi.updatePassword({
+		return await this.__getSubscriptionData(
+			this.__userRestApi.updatePassword({
 				email: this.__currentUser.email,
 				password: this.__currentUser.password,
 				newPassword: this.__currentUser.newPassword,
 				sessionID: this.__sessionIDLocalStorage.get()
-			});
-
-			let subscription = observable.subscribe((result) => {
-				returnData(result);
-				subscription.unsubscribe();
-			});
-		});
+			})
+		);
 	}
 
 
 	async updateEmail(): Promise<any> {
-		return new Promise((returnData) => {
-
-			let observable = this.__userRestApi.updateEmail({
+		return await this.__getSubscriptionData(
+			this.__userRestApi.updateEmail({
 				email: this.__currentUser.email,
 				password: this.__currentUser.password,
 				newEmail: this.__currentUser.newEmail,
 				sessionID: this.__sessionIDLocalStorage.get()
-			});
+			})
+		);
+	}
 
+
+	private async __getSubscriptionData(observable): Promise<any> {
+		return new Promise((returnData) => {
 			let subscription = observable.subscribe((result) => {
 				returnData(result);
 				subscription.unsubscribe();
 			});
 		});
+
 	}
 
 
