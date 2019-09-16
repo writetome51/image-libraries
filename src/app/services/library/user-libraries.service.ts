e9c63ae8060b66d94263bcf7db177bf9c3543346
Steve Thompson <writetome51@gmail.com>
2019-09-16 14:33:20 -0600

    Added error handling to UserLibrariesService

--- src/app/services/library/user-libraries.service.ts
+++ src/app/services/library/user-libraries.service.ts
@@ -1,6 +1,8 @@
+import { getObjectFromJSON } from 'get-object-from-json';
 import { Injectable } from '@angular/core';
 import { PublicArrayContainer } from '@writetome51/public-array-container';
 import { LibraryStorageService } from './library-storage.service';
+import { SuccessOrErrorMessageService } from '../success-or-error-message.service';
 
 
 @Injectable({
@@ -9,7 +11,10 @@ import { LibraryStorageService } from './library-storage.service';
 export class UserLibrariesService extends PublicArrayContainer {
 
 
-	constructor(private __libraryStorage: LibraryStorageService) {
+	constructor(
+		private __libraryStorage: LibraryStorageService,
+		private __successOrErrorMessage: SuccessOrErrorMessageService
+	) {
 		super();
 
 		this.__set_data();
@@ -17,7 +22,12 @@ export class UserLibrariesService extends PublicArrayContainer {
 
 
 	private async __set_data() {
-		this.data = await this.__libraryStorage.getLibraries();
+		let result = await this.__libraryStorage.getLibraries();
+
+		if (typeof result === 'string') result = getObjectFromJSON(result);
+
+		if (result.error) this.__successOrErrorMessage.error = result.error.message;
+		else this.data = result;
 	}
 
 
