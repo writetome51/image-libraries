a1b83b17ea7a4f1e51d8df8b77cc31a734aad4b0
Steve Thompson <writetome51@gmail.com>
2019-10-02 13:12:33 -0600

    Minor changes to LoadingSpinnerComponent

--- src/app/loading-spinner/loading-spinner.component.ts
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -3,12 +3,22 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'loading-spinner',
-	templateUrl: './loading-spinner.component.html'
+	template: `<img [src]="spinner400.src" [width]="spinner400.width" alt="waiting for response"/>`,
+	styles: ['img{z-index: 1000; position: fixed;}']
 })
 export class LoadingSpinnerComponent {
 
-	constructor() {
-	}
+	spinner400 = {
+		src: '../../assets/loading-spinner-400.gif',
+		width: 407,
+		height: 406,
+	};
+
+	spinner200 = {
+		src: '../../assets/loading-spinner-200.gif',
+		width: 204,
+		height: 203,
+	};
 
 
 }
