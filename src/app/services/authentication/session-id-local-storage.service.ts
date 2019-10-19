ede242ff36086183ea019e3d3cd40091eb96d635
Steve Thompson <writetome51@gmail.com>
2019-10-19 14:27:10 -0600

    Minor change to SessionIDLocalStorageService

--- src/app/services/authentication/session-id-local-storage.service.ts
+++ src/app/services/authentication/session-id-local-storage.service.ts
@@ -1,9 +1,9 @@
+import { ecky } from '../../../assets/.ecky';
 import { hasValue } from '@writetome51/has-value-no-value';
 import { Injectable } from '@angular/core';
 import { ObjectInLocalStorage } from '@writetome51/object-in-local-storage';
 import { SimpleCrypto } from 'simple-crypto-js';
 // if above import doesn't work, try:  const SimpleCrypto = require("simple-crypto-js").default;
-import { ecky } from '../../../assets/.ecky';
 
 
 @Injectable({
@@ -15,33 +15,33 @@ export class SessionIDLocalStorageService {
 	// Since it is unique, and also stored in the database, the app uses this to check if he is
 	// still logged in.
 
-	private __localCurrentUser = new ObjectInLocalStorage();
+	private __localObject = new ObjectInLocalStorage();
 	private __cryptographer = new SimpleCrypto(ecky);
 
 
 	constructor() {
-		this.__localCurrentUser.key = 'image-libraries-current-user';
+		this.__localObject.key = 'image-libraries-current-user';
 	}
 
 
 	set(sessionID): void {
 		sessionID = this.__cryptographer.encrypt(sessionID);
-		this.__localCurrentUser.set({sessionID});
+		this.__localObject.set({sessionID});
 	}
 
 
 	get(): string {
-		let localCurrentUser = this.__localCurrentUser.get();
-		if (hasValue(localCurrentUser)) {
+		let localObject = this.__localObject.get();
+		if (hasValue(localObject)) {
 			// @ts-ignore
-			return this.__cryptographer.decrypt(localCurrentUser['sessionID']);
+			return this.__cryptographer.decrypt(localObject['sessionID']);
 		}
 		else return '';
 	}
 
 
 	remove() {
-		this.__localCurrentUser.remove();
+		this.__localObject.remove();
 	}
 
 
