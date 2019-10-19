cba2dd9a9c877029160198c3a5910d7418e04adc
Steve Thompson <writetome51@gmail.com>
2019-10-19 13:54:37 -0600

    Now when DataOperationBegunService.waitingForResult is set to true, SuccessOrErrorMessageService.message is erased, and any visible success or error message shown in a page will disappear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreateUserFormComponent } from './create-user/create-user-form/create-user-form.component';
+import { EditUserModule } from './edit-user/edit-user.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { IntroductionComponent } from './introduction/introduction.component';
@@ -13,7 +14,6 @@ import { LoginFormComponent } from './introduction/login-form/login-form.compone
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { SuccessOrErrorMessageModule } from './success-or-error-message/success-or-error-message.module';
-import { EditUserModule } from './edit-user/edit-user.module';
 
 
 @NgModule({
--- src/app/services/data-operation-begun.service.ts
+++ src/app/services/data-operation-begun.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { SuccessOrErrorMessageService } from './success-or-error-message.service';
 
 
 @Injectable({
@@ -9,6 +10,23 @@ import { Injectable } from '@angular/core';
 
 export class DataOperationBegunService {
 
-	waitingForResult = false; // if true, you should show loading spinner.
+	private __waitingForResult = false; // if true, you should show loading spinner.
+
+
+	constructor(private __successOrErrorMessage: SuccessOrErrorMessageService) {
+	}
+
+
+	get waitingForResult(): boolean {
+		return this.__waitingForResult;
+	}
+
+
+	set waitingForResult(value) {
+		// Whenever a new data operation is begun, any message should be removed:
+		if (value === true) this.__successOrErrorMessage.success = '';
+
+		this.__waitingForResult = value;
+	}
 
 }
