e1c2e91d58b8d8386d046c97354e5df85983754d
Steve Thompson <writetome51@gmail.com>
2019-10-25 14:11:00 -0600

    Added "minLength" attribute to form inputs in LoginFormComponent html template

--- src/app/introduction/login-form/login-form.component.ts
+++ src/app/introduction/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CurrentUserService } from '../../services/user/current-user.service';
 import { LoginProcessorService } from '../../services/authentication/login-processor/login-processor.service';
 import { DataOperationBegunService } from '../../services/data-operation-begun.service';
+import { UserValidationRulesService } from '../../services/user/user-validation-rules.service';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class LoginFormComponent implements OnInit{
 
 	constructor(
 		public currentUser: CurrentUserService,
+		public userValidationRules: UserValidationRulesService,
 		public dataOperationBegun: DataOperationBegunService,
 		private __loginProcessor: LoginProcessorService
 	) {
