e3221366abdbcb7cb4c151e95ce2f3f0b902384d
Steve Thompson <writetome51@gmail.com>
2019-10-28 16:47:35 -0600

    Created ChangePasswordComponent. Created ForgotPasswordComponent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { AlertModule } from './alert/alert.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreateUserFormComponent } from './create-user/create-user-form/create-user-form.component';
 import { CreateUserFormInputsComponent }
 	from './create-user/create-user-form/create-user-form-inputs/create-user-form-inputs.component';
 import { EditUserModule } from './edit-user/edit-user.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { IntroductionComponent } from './introduction/introduction.component';
-import { LibraryModule } from './library/library.module';
 import { LibrariesComponent } from './libraries/libraries.component';
+import { LibraryModule } from './library/library.module';
 import { LoadingSpinnerModule } from './loading-spinner/loading-spinner.module';
 import { LoginFormComponent } from './introduction/login-form/login-form.component';
 import { NgModule } from '@angular/core';
@@ -27,7 +29,9 @@ import { PageNotFoundComponent } from './page-not-found.component';
 		IntroductionComponent,
 		LibrariesComponent,
 		LoginFormComponent,
-		PageNotFoundComponent
+		PageNotFoundComponent,
+		ForgotPasswordComponent,
+		ChangePasswordComponent
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/change-password/change-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'change-password',
+  templateUrl: './change-password.component.html'
+})
+export class ChangePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
