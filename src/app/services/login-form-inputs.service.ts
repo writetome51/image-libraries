79a5903cecefea4eb03d341b2d32375ade2a9c1a
Steve Thompson <writetome51@gmail.com>
2019-10-30 12:24:03 -0600

    Created LoginFormInputsService, extending from ValidatingInputsService

---
+++ src/app/services/login-form-inputs.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { ValidatingInputsService } from './validating-inputs.service';
+import { CurrentUserService } from './user/current-user.service';
+import { UserValidationRulesService } from './user/user-validation-rules.service';
+
+
+@Injectable({
+	providedIn: 'root'
+})
+export class LoginFormInputsService extends ValidatingInputsService {
+
+
+	constructor(
+		private __currentUser: CurrentUserService,
+		private __rules: UserValidationRulesService
+	) {
+		super();
+
+		this.data = [
+			{
+				type: 'text',
+				id: 'email-input',
+				propertyToBind: this.__currentUser.email,
+				placeholder: 'Email',
+				required: true,
+				isValid: this.__currentUser.email.length >= this.__rules.emailMinLength,
+				errorMessage:
+					`The email must be at least ${this.__rules.emailMinLength} characters`,
+				error: '', // If input is invalid, this is assigned value of errorMessage.
+			},
+			{
+				type: 'password',
+				id: 'password-input',
+				propertyToBind: this.__currentUser.password,
+				placeholder: 'Password',
+				required: true,
+				isValid: this.__currentUser.password.length >= this.__rules.passwordMinLength,
+				errorMessage:
+					`The password must be at least ${this.__rules.passwordMinLength} characters`,
+				error: '',
+			}
+		];
+	}
+
+
+}
