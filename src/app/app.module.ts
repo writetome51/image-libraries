7e3f1f918dec0b6f011de5cc7a8d609fa2fbc122
Steve Thompson <writetome51@gmail.com>
2019-11-01 19:28:51 -0600

    Removed unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChangePasswordComponent } from './change-password/change-password.component';
-import { EditUserModule } from './edit-user/edit-user.module';
+import { EmailPasswordFormInputsModule }
+	from './email-password-form-inputs/email-password-form-inputs.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { LibrariesComponent } from './libraries/libraries.component';
-import { LibraryModule } from './library/library.module';
 import { LoadingSpinnerModule } from './loading-spinner/loading-spinner.module';
 import { LoginFormComponent } from './introduction/login-form/login-form.component';
-import { NewUserModule } from './new-user/new-user.module';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ValidatingInputsModule } from './validating-inputs/validating-inputs.module';
-import { EmailPasswordFormInputsModule }
-	from './email-password-form-inputs/email-password-form-inputs.module';
 
 
 @NgModule({
@@ -39,10 +36,6 @@ import { EmailPasswordFormInputsModule }
 		ValidatingInputsModule,
 		EmailPasswordFormInputsModule,
 		AppRoutingModule, // must be loaded before any other routing module.
-		LibraryModule,
-		NewUserModule,
-		EditUserModule,
-
 	],
 	bootstrap: [AppComponent]
 })
