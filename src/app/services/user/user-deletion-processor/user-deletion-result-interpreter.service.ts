dbe5d5d752dec1ccbfaad7787c088704461f1278
Steve Thompson <writetome51@gmail.com>
2019-11-02 19:56:36 -0600

    UserDeletionResultInterpreterService now extends from ResultInterpreterService. UserDeletorService received a minor change

--- src/app/services/user/user-deletion-processor/user-deletion-result-interpreter.service.ts
+++ src/app/services/user/user-deletion-processor/user-deletion-result-interpreter.service.ts
@@ -4,30 +4,27 @@ import { OperationRequiringEmailPasswordResultService }
 	from '../../data-operation-result/operation-requiring-email-password-result.service';
 import { UserLogoutOrDeletionResultProcessorService }
 	from '../user-logout-or-deletion-result-processor.service';
-import { ResultInterpreter } from '../../../../interfaces/result-interpreter';
+import { ResultInterpreterService } from '../../result-interpreter.service';
 
 
 @Injectable({
 	providedIn: 'root'
 })
-export class UserDeletionResultInterpreterService implements ResultInterpreter {
+export class UserDeletionResultInterpreterService extends ResultInterpreterService {
+
 
 	constructor(
 		private __alert: AlertService,
-		private __userDeletionResult: OperationRequiringEmailPasswordResultService,
-		private __userDeletionResultProcessor: UserLogoutOrDeletionResultProcessorService
+		__userDeletionResult: OperationRequiringEmailPasswordResultService,
+		__userDeletionResultProcessor: UserLogoutOrDeletionResultProcessorService
 	) {
+		super(__userDeletionResult, __userDeletionResultProcessor);
+
+		this._process = (result) => {
+			super._process(result);
+			this.__alert.success = 'Account successfully deleted';
+		};
 	}
 
-
-	async interpret(result) {
-		await this.__userDeletionResult.ifSuccessful_processResult(
-			result,
-			(result) => {
-				this.__userDeletionResultProcessor.process(result);
-				this.__alert.success = 'Account successfully deleted';
-			}
-		);
-	}
 
 }
--- src/app/services/user/user-deletion-processor/user-deletor.service.ts
+++ src/app/services/user/user-deletion-processor/user-deletor.service.ts
@@ -1,25 +1,26 @@
 import { CurrentUserService } from '../current-user.service';
-import { getSubscriptionData } from '@writetome51/get-subscription-data';
 import { Injectable } from '@angular/core';
 import { SessionIDLocalStorageService } from '../../authentication/session-id-local-storage.service';
 import { UserRestAPIService } from '../user-rest-api.service';
+import { ReturnObjectFromSubscriptionService } from '../../return-object-from-subscription.service';
 
 
 @Injectable({
 	providedIn: 'root'
 })
-export class UserDeletorService {
+export class UserDeletorService extends ReturnObjectFromSubscriptionService {
 
 	constructor(
 		private __currentUser: CurrentUserService,
 		private __userRestApi: UserRestAPIService,
 		private __sessionIDLocalStorage: SessionIDLocalStorageService,
 	) {
+		super();
 	}
 
 
 	async delete(): Promise<any> {
-		return await getSubscriptionData(
+		return await this._getObjectFromSubscriptionTo(
 			this.__userRestApi.delete({
 				email: this.__currentUser.email,
 				password: this.__currentUser.password,
