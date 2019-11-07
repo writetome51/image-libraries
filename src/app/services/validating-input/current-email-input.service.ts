7d65e7645940cf1e605d200b4e6d5da9472c2446
Steve Thompson <writetome51@gmail.com>
2019-11-06 23:31:04 -0700

    Reverted services/validating-input directory back to before NewDataInputService and CurrentDataInputService were created

--- src/app/services/validating-input/current-email-input.service.ts
+++ src/app/services/validating-input/current-email-input.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
+import { CurrentUserService } from '../user/current-user.service';
+import { UserValidationRulesService } from '../user/user-validation-rules.service';
 import { EmailInputService } from './email-input.service';
-import { CurrentInputService } from './current-input.service';
 
 
 @Injectable({
 	providedIn: 'root'
 })
-export class CurrentEmailInputService extends CurrentInputService {
+export class CurrentEmailInputService extends EmailInputService {
 
-	constructor(__emailInput: EmailInputService) {
-		super(__emailInput);
+	constructor(
+		__currentUser: CurrentUserService,
+		__rules: UserValidationRulesService
+	) {
+		super(__currentUser, __rules);
+
+		this.data.id = 'current-email-input';
+		this.data.placeholder = 'Current Email';
+		this.data.errorMessage = 'The current ' + this.data.errorMessage.slice(4);
 	}
 
 }
--- src/app/services/validating-input/current-password-input.service.ts
+++ src/app/services/validating-input/current-password-input.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CurrentInputService } from './current-input.service';
+import { CurrentUserService } from '../user/current-user.service';
+import { UserValidationRulesService } from '../user/user-validation-rules.service';
 import { PasswordInputService } from './password-input.service';
 
 
 @Injectable({
 	providedIn: 'root'
 })
-export class CurrentPasswordInputService extends CurrentInputService {
+export class CurrentPasswordInputService extends PasswordInputService {
 
-	constructor(__passwordInput: PasswordInputService) {
-		super(__passwordInput);
+	constructor(
+		__currentUser: CurrentUserService,
+		__rules: UserValidationRulesService
+	) {
+		super(__currentUser, __rules);
+
+		this.data.id = 'current-password-input';
+		this.data.placeholder = 'Current Password';
+		this.data.errorMessage = 'The current ' + this.data.errorMessage.slice(4);
 	}
 
 }
