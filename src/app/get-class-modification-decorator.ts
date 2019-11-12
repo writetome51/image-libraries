178f5b477ef5e3d71eb8d0469e45092cda62fae7
Steve Thompson <writetome51@gmail.com>
2019-11-12 16:30:59 -0700

    Added comments with usage example to getClassModificationDecorator()

--- src/app/get-class-modification-decorator.ts
+++ src/app/get-class-modification-decorator.ts
@@ -1,3 +1,28 @@
+/*********************
+Returns a TypeScript class decorator.
+param `modifyInstance()` is called inside the constructor of the class being decorated,
+allowing you to change its behavior.
+Usage example:
+
+export const attach_prefix = getClassDecorator(
+ 	(instance: Employee, decoratorArgs: [string]) => {
+		let prefix = decoratorArgs[0];
+		instance.name = prefix + ' ' + instance.name;
+	}
+);
+
+export class Employee {
+	name = 'employee';
+}
+
+@attach_prefix('subordinate')
+export class Subordinate extends Employee {
+}
+
+let subordinate = new Subordinate();
+console.log(subordinate.name); // 'subordinate employee'
+*********************/
+
 export function getClassModificationDecorator(
 	modifyInstance: (instance, decoratorArgs: any[]) => void
 ) {
