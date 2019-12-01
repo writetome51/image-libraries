bb61e539c19ee0016d6641f9d700e18f9e5185d9
Steve Thompson <writetome51@gmail.com>
2019-11-30 17:21:22 -0700

    minor changes

--- src/app/security-question/answer-security-question-form/answer-security-question-form.component.ts
+++ src/app/security-question/answer-security-question-form/answer-security-question-form.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
-  selector: 'answer-security-question-form',
-  templateUrl: './answer-security-question-form.component.html',
-  styleUrls: ['./answer-security-question-form.component.sass']
+	selector: 'answer-security-question-form',
+	templateUrl: './answer-security-question-form.component.html',
+	styleUrls: ['./answer-security-question-form.component.sass']
 })
-export class AnswerSecurityQuestionFormComponent implements OnInit {
+export class AnswerSecurityQuestionFormComponent {
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
 
-  submitAnswer(){}
+	submitAnswer() {
+	}
 
 }
