6ae0d67f3970859cc6f8a1435c368b6df71fcb78
Steve Thompson <writetome51@gmail.com>
2019-12-05 17:34:37 -0700

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginFormInputsComponent }
-	from './login/login-form/login-form-inputs.component';
 import { LibrariesComponent } from './libraries/libraries.component';
 import { LoginComponent } from './login/login.component';
+import { LoginFormInputsComponent } from './login/login-form/login-form-inputs.component';
 import { LoginFormComponent } from './login/login-form/login-form.component';
 import { LogoutButtonComponent } from './logout-button.component';
+import { LoginButtonComponent } from './login/login-form/login-button.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { ProcessingButtonModule } from './processing-button/processing-button.module';
 import { ValidatingInputsModule } from './validating-inputs/validating-inputs.module';
-import { LoginButtonComponent } from './login/login-form/login-button.component';
 
 
 @NgModule({
