b8a272da54d3b0077b23f3fe445369ef23e99772
Steve Thompson <writetome51@gmail.com>
2020-02-20 02:01:41 -0700

    Minor changes to UploadedImagesProcessor

--- src/app/services/image/uploaded-images-processor.service.ts
+++ src/app/services/image/uploaded-images-processor.service.ts
@@ -1,40 +1,45 @@
 import { DirectProcessor } from '../../interfaces/direct-processor';
 import { getDataURL } from '@writetome51/get-data-url';
 import { Injectable } from '@angular/core';
-import { DisplayedImagesService } from './displayed-images.service';
 import { AppImage } from '../../interfaces/app-image';
+import { ImagesRestApiService } from './images-rest-api.service';
+import { LocalSessionIDService } from '../local-data/local-session-id.service';
 
 
 @Injectable({providedIn: 'root'})
 
 export class UploadedImagesProcessorService implements DirectProcessor {
 
+
 	constructor(
-		private __imagesToDisplay: DisplayedImagesService
+		private __imagesRestApi: ImagesRestApiService,
+		private __localSessionID: LocalSessionIDService
 	) {
 	}
 
 
 	async process(files: FileList | File[]): Promise<void> {
+		let images = [];
 
 		for (let i = 0; i < files.length; ++i) {
-			await this.__addTo__imagesToDisplay(files[i]);
+			images[i] = await this.__getAppImage(files[i]);
 		}
+		this.__imagesRestApi.add(
+			{sessionID: this.__localSessionID.get(), images}
+		);
 	}
 
 
-	private async __addTo__imagesToDisplay(file) {
-
-		let image: AppImage = {
+	private async __getAppImage(file): Promise<AppImage> {
+		return {
 			name: file.name,
 			src: await getDataURL(file),
 			description: '',
 			tags: [],
 			date: new Date(file.lastModified),
-			location: {latitude: undefined, longitude: undefined}
+			location: {latitude: undefined, longitude: undefined},
+			rating: undefined
 		};
-
-		this.__imagesToDisplay.data.push(image);
 	}
 
 
