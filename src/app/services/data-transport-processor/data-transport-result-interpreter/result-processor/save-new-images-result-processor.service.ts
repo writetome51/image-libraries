34183163fb287ddcfaa30ce9449ff52619c3e6ff
Steve Thompson <writetome51@gmail.com>
2020-02-29 21:21:32 -0700

    Now whenever you save new image(s) to your account you are redirected to the libraries/all-images route

--- src/app/services/data-transport-processor/data-transport-result-interpreter/result-processor/save-new-images-result-processor.service.ts
+++ src/app/services/data-transport-processor/data-transport-result-interpreter/result-processor/save-new-images-result-processor.service.ts
@@ -3,20 +3,34 @@ import { DirectProcessor } from '../../../../interfaces/direct-processor';
 import { AlertService as alert } from '../../../alert.service';
 import { GetAllImagesProcessorService } from '../../get-all-images-processor.service';
 import { NewImagesData as newImages } from '../../../../data/runtime-state-data/new-images.data';
+import { Router } from '@angular/router';
+import { LibrariesModuleRouteService as moduleRoute }
+	from '../../../../libraries/libraries-module-route.service';
+import { AppModuleRouteService } from '../../../../app-module-route.service';
 
 
 @Injectable({providedIn: 'root'})
 
 export class SaveNewImagesResultProcessorService implements DirectProcessor {
 
-	constructor(private __getAllImagesProcessor: GetAllImagesProcessorService) {
+	constructor(
+		private __getAllImagesProcessor: GetAllImagesProcessorService,
+		private __router: Router
+	) {
 	}
 
 
 	async process(result) {
-		await this.__getAllImagesProcessor.process(); // refreshes AllImagesData.data
+		await this.__getAllImagesProcessor.process(); // refreshes LoadedImagesData.data
 		newImages.data = [];
+
+		await this.__router.navigate([this.__getRouteTo_AllImagesComponent()]);
 		alert.success = 'New images saved';
+	}
+
+
+	private __getRouteTo_AllImagesComponent() {
+		return AppModuleRouteService.LibrariesModule + '/' + moduleRoute.AllImagesComponent;
 	}
 
 }
