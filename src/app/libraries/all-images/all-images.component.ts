68d2bffb066a021af05c5e54720caef8457a9a78
Steve Thompson <writetome51@gmail.com>
2020-03-08 00:20:50 -0700

    Added "next page" link to test pagination. AllImagesComponent now has subscription to CurrentRouteService.params$ observable so its paginator can update the page whenever the "pageNumber" parameter in the URL changes.

--- src/app/libraries/all-images/all-images.component.ts
+++ src/app/libraries/all-images/all-images.component.ts
@@ -1,24 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DBImage } from '../../interfaces/db-image';
 import { noValue, hasValue } from '@writetome51/has-value-no-value';
+import { not } from '@writetome51/not';
 import { LoadedImagesData as loadedImages }
 	from '../../data/runtime-state-data/static-classes/loaded-images.data';
 import { AllImagesPaginatorService } from '../../services/paginator/all-images-paginator.service';
+import { AllImagesStatusData as allImagesStatus }
+	from '../../data/runtime-state-data/static-classes/all-images-status.data';
+import { URLParamIDData as paramID } from '../../data/read-only-data/url-param-id.data';
+import { CurrentRouteService } from '../../services/current-route.service';
+import { Subscription } from 'rxjs';
+import { OperationStatusData } from '../../data/runtime-state-data/operation-status.data';
 
 
 @Component({
 	selector: 'all-images',
 	templateUrl: './all-images.component.html'
 })
-export class AllImagesComponent {
+export class AllImagesComponent implements OnInit, OnDestroy {
 
-	get images(): DBImage[] {
-		if (hasValue(loadedImages.data)) return Object.values(loadedImages.data);
+	private __images;
+	private __page: number;
+	private __routeParamsSubscription: Subscription;
+
+
+	get gettingBatch(): boolean {
+		return OperationStatusData.waiting;
 	}
 
 
-	constructor(private __allImagesPaginator: AllImagesPaginatorService) {
-		if (noValue(loadedImages.data)) this.__allImagesPaginator.reset();
+	get images(): DBImage[] {
+		return this.__images;
+	}
+
+
+	get nextPage() {
+		return this.__page + 1;
+	}
+
+
+	constructor(
+		private __allImagesPaginator: AllImagesPaginatorService,
+		private __currentRoute: CurrentRouteService
+	) {
+	}
+
+
+	async ngOnInit() {
+		if (noValue(loadedImages.data) || not(allImagesStatus.loaded)) {
+			await this.__allImagesPaginator.reset();
+			allImagesStatus.loaded = true;
+		}
+
+		this.__routeParamsSubscription = this.__currentRoute.params$.subscribe(
+			async (params) => {
+				this.__page = Number(params[paramID.pageNumber]);
+				await this.__allImagesPaginator.set_currentPageNumber(this.__page);
+				this.__images = this.__allImagesPaginator.currentPage;
+			}
+		);
+	}
+
+
+	ngOnDestroy(): void {
+		this.__routeParamsSubscription.unsubscribe();
 	}
 
 }
