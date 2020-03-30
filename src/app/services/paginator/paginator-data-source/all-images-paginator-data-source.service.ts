e4b6a2ada8f3e6f501ccc6e16dbe8b2092ee8c23
Steve Thompson <writetome51@gmail.com>
2020-03-29 23:36:09 -0600

    new commit

--- src/app/services/paginator/paginator-data-source/all-images-paginator-data-source.service.ts
+++ src/app/services/paginator/paginator-data-source/all-images-paginator-data-source.service.ts
@@ -2,20 +2,11 @@ import { Injectable } from '@angular/core';
 import { GetAllImagesProcessorService }
 	from '../../data-transport-processor/image-fetching-processor/get-all-images-processor.service';
 import { PaginatorDataSourceService } from './paginator-data-source.service';
-import { hasValue } from '@writetome51/has-value-no-value';
 import { SetInitialDataTotalForAllImagesService }
 	from './set-initial-data-total-for-all-images.service';
 import { DBImage } from '../../../interfaces/db-image';
-import { BatchSizeData as batch }
-	from '../../../data-structures/runtime-state-data/static-classes/batch-size.data';
-import { PerformDataProcessRequiringWaitingService as performDataProcessRequiringWaiting }
-	from '../../perform-data-process-requiring-waiting.service';
-import { OperationStatusData as operationStatus }
-	from '../../../data-structures/runtime-state-data/operation-status.data';
 import { LoadedImagesData as loadedImages }
 	from '../../../data-structures/runtime-state-data/static-classes/loaded-images.data';
-import { LoadedLibraryData as loadedLibrary }
-	from '../../../data-structures/runtime-state-data/static-classes/loaded-library.data';
 
 
 @Injectable({providedIn: 'root'})
