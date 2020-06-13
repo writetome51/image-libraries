f0a3b03ecf23a197de30a008fcf42c753021ca04
Steve Thompson <writetome51@gmail.com>
2020-06-12 21:37:10 -0600

    minor changes

--- src/app/new-user/new-user-form/new-user-form.component.ts
+++ src/app/new-user/new-user-form/new-user-form.component.ts
@@ -3,7 +3,19 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'new-user-form',
-	templateUrl: './new-user-form.component.html'
+	template: `
+		<div class="form-container">
+
+			<form>
+
+				<new-user-form-inputs></new-user-form-inputs>
+
+				<create-user-button></create-user-button>
+
+			</form>
+
+		</div>
+	`
 })
 export class NewUserFormComponent {
 }
