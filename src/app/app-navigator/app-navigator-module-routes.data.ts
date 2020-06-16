6b840bd80ccc2d143755fbd792e34b655a71998b
Steve Thompson <writetome51@gmail.com>
2020-06-16 16:07:36 -0600

    very minor change

--- src/app/app-navigator/app-navigator-module-routes.data.ts
+++ src/app/app-navigator/app-navigator-module-routes.data.ts
@@ -1,6 +1,5 @@
 // Contains all routes the AppNavigatorModule is concerned with.
-// The keys are the modules or components that will be loaded when
-// their route is accessed.
+// The keys are the modules that will be loaded when their route is accessed.
 
 
 export class AppNavigatorModuleRoutesData {
