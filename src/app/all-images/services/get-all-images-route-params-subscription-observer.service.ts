5e7ac67226da425d1301834363532e0278aa48ba
Steve Thompson <writetome51@gmail.com>
2020-06-23 22:05:39 -0600

    minor changes

--- src/app/all-images/services/get-all-images-route-params-subscription-observer.service.ts
+++ src/app/all-images/services/get-all-images-route-params-subscription-observer.service.ts
@@ -1,19 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AllImagesPaginatorService } from '../../services/app-paginator/all-images-paginator.service';
-import { LoadedImagesData as loadedImages }
-	from '../../../data-structures/runtime-state-data/static-classes/loaded-images.data';
-import { LoadedImagesStatusData as loadedImagesStatus }
-	from '../../../data-structures/runtime-state-data/static-classes/loaded-images-status.data';
 import { OperationStatusData as operationStatus }
 	from '../../../data-structures/runtime-state-data/operation-status.data';
-import { noValue } from '@writetome51/has-value-no-value';
-import { not } from '@writetome51/not';
-import { AllImagesJumpToPageNumberInputService }
-	from '../../services/validating-input/jump-to-page-number/all-images-jump-to-page-number-input.service';
-import { AllImagesRouteParamsHandlerService } from './all-images-route-params-handler.service';
-import { LoadedLibraryData as loadedLibrary }
-	from '../../../data-structures/runtime-state-data/static-classes/loaded-library.data';
 import { IDoThis } from '../../../interfaces/i-do-this';
+import { RunTasksNeededAfterAllImagesRouteParamsReceivedService }
+	from './run-tasks-needed-after-all-images-route-params-received.service';
 
 
 @Injectable({providedIn: 'root'})
@@ -22,9 +12,8 @@ export class GetAllImagesRouteParamsSubscriptionObserverService implements IDoTh
 
 
 	constructor(
-		private __allImagesPaginator: AllImagesPaginatorService,
-		private __allImagesJumpToPageNumberInput: AllImagesJumpToPageNumberInputService,
-		private __routeParamsHandler: AllImagesRouteParamsHandlerService
+		private __runTasksNeededAfterAllImagesRouteParamsReceived:
+			RunTasksNeededAfterAllImagesRouteParamsReceivedService
 	) {
 	}
 
@@ -33,15 +22,7 @@ export class GetAllImagesRouteParamsSubscriptionObserverService implements IDoTh
 
 		return async (params) => {
 			operationStatus.waiting = true;
-
-			if (noValue(loadedImages.data) || not(loadedImagesStatus.data === 'all')) {
-				await this.__allImagesPaginator.resetToFirstPage();
-				this.__allImagesJumpToPageNumberInput.setMax();
-				loadedImagesStatus.data = 'all';
-				loadedLibrary.data = undefined;
-			}
-
-			await this.__routeParamsHandler.handle(params);
+			await this.__runTasksNeededAfterAllImagesRouteParamsReceived.go(params);
 			operationStatus.waiting = false;
 		};
 
---
+++ src/app/all-images/services/run-tasks-needed-after-all-images-route-params-received.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { AllImagesPaginatorService } from '../../services/app-paginator/all-images-paginator.service';
+import { LoadedImagesData as loadedImages }
+	from '../../../data-structures/runtime-state-data/static-classes/loaded-images.data';
+import { LoadedImagesStatusData as loadedImagesStatus }
+	from '../../../data-structures/runtime-state-data/static-classes/loaded-images-status.data';
+import { noValue } from '@writetome51/has-value-no-value';
+import { not } from '@writetome51/not';
+import { AllImagesJumpToPageNumberInputService }
+	from '../../services/validating-input/jump-to-page-number/all-images-jump-to-page-number-input.service';
+import { AllImagesRouteParamsHandlerService } from './all-images-route-params-handler.service';
+import { LoadedLibraryData as loadedLibrary }
+	from '../../../data-structures/runtime-state-data/static-classes/loaded-library.data';
+import { IDoThis } from '../../../interfaces/i-do-this';
+
+
+@Injectable({providedIn: 'root'})
+
+export class RunTasksNeededAfterAllImagesRouteParamsReceivedService implements IDoThis {
+
+
+	constructor(
+		private __allImagesPaginator: AllImagesPaginatorService,
+		private __allImagesJumpToPageNumberInput: AllImagesJumpToPageNumberInputService,
+		private __routeParamsHandler: AllImagesRouteParamsHandlerService
+	) {
+	}
+
+
+	async go(params): Promise<void> {
+
+		if (noValue(loadedImages.data) || not(loadedImagesStatus.data === 'all')) {
+			await this.__allImagesPaginator.resetToFirstPage();
+			this.__allImagesJumpToPageNumberInput.setMax();
+			loadedImagesStatus.data = 'all';
+			loadedLibrary.data = undefined;
+		}
+
+		await this.__routeParamsHandler.handle(params);
+	}
+
+
+}
