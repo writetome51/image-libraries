2e81468e291806e9e69f196b5a726fe9458d9849
Steve Thompson <writetome51@gmail.com>
2020-07-23 01:33:35 -0600

    CurrentUserInputsService no longer extends AppValidatingInputsService. AppValidatingInputsService has been deleted.

--- src/app/shared/services/validating-inputs/current-user-inputs.service.ts
+++ src/app/shared/services/validating-inputs/current-user-inputs.service.ts
@@ -1,11 +1,11 @@
-import { AppValidatingInputsService } from './app-validating-inputs.service';
 import { Bind_ValidatingInputsService_to_Object }
 	from './bind-validating-inputs-service-to-object/bind-validating-inputs-service-to-object.decorator';
 import { CurrentUserData }
 	from '../../../../data-structures/runtime-state-data/static-classes/current-user.data';
+import { ValidatingInputsService } from '@writetome51/validating-inputs';
 
 
 @Bind_ValidatingInputsService_to_Object(CurrentUserData)
 
-export abstract class CurrentUserInputsService extends AppValidatingInputsService {
+export abstract class CurrentUserInputsService extends ValidatingInputsService {
 }
