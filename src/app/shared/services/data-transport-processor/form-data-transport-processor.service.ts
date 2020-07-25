21b05b4ae82937c0f2ac14d94a5568cd0f851c42
Steve Thompson <writetome51@gmail.com>
2020-07-25 02:03:54 -0600

    FormDataTransportProcessorService now allows you to pass instance of ValidatingInputService instead of ValidatingInputsService.  You can pass either one.  If input or inputs are not valid, it assigns the error message to the AlertData.

--- src/app/shared/services/data-transport-processor/form-data-transport-processor.service.ts
+++ src/app/shared/services/data-transport-processor/form-data-transport-processor.service.ts
@@ -1,13 +1,15 @@
 import { DataTransportResultInterpreterService }
 	from './data-transport-result-interpreter/data-transport-result-interpreter.service';
-import { ValidatingInputsService } from '@writetome51/validating-inputs';
+import { ValidatingInputService, ValidatingInputsService } from '@writetome51/validating-inputs';
 import { DataTransportProcessorService } from './data-transport-processor.service';
+import { AlertData as alert }
+	from '../../../../data-structures/runtime-state-data/static-classes/alert.data';
 
 
 export abstract class FormDataTransportProcessorService extends DataTransportProcessorService {
 
 	constructor(
-		private __validatingInputs: ValidatingInputsService,
+		private __validatingInputs: ValidatingInputsService | ValidatingInputService,
 		__resultInterpreter: DataTransportResultInterpreterService
 	) {
 		super(__resultInterpreter);
@@ -15,9 +17,15 @@ export abstract class FormDataTransportProcessorService extends DataTransportPro
 
 
 	async process() {
-		if (this.__validatingInputs.areValid()) {
+		if (
+			(this.__validatingInputs instanceof ValidatingInputsService
+				&& this.__validatingInputs.areValid())
+			|| (this.__validatingInputs instanceof ValidatingInputService
+			&& this.__validatingInputs.isValid())
+		) {
 			await super.process();
 		}
+		else alert.error = this.__validatingInputs.error;
 	}
 
 }
