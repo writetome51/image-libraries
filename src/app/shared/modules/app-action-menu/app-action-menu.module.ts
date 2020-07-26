b01fe5278c464c9166d0fda738139d9081cd052c
Steve Thompson <writetome51@gmail.com>
2020-07-25 19:35:55 -0600

    Minor changes

--- src/app/shared/modules/app-action-menu/app-action-menu.module.ts
+++ src/app/shared/modules/app-action-menu/app-action-menu.module.ts
@@ -2,15 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AppActionMenuComponent } from './app-action-menu.component';
 import { MenuChoiceComponent } from './menu-choice/menu-choice.component';
-import { StartDataProcessListItemModule }
-	from '../start-data-process-list-item/start-data-process-list-item.module';
+import { ClickStartDataProcessListItemModule }
+	from '../click-start-data-process-list-item/click-start-data-process-list-item.module';
 
 
 @NgModule({
 	declarations: [AppActionMenuComponent, MenuChoiceComponent],
 	imports: [
 		CommonModule,
-		StartDataProcessListItemModule,
+		ClickStartDataProcessListItemModule,
 	],
 	exports: [AppActionMenuComponent]
 })
--- src/app/shared/modules/click-start-data-process-list-item/click-start-data-process-list-item.module.ts
+++ src/app/shared/modules/click-start-data-process-list-item/click-start-data-process-list-item.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ClickStartDataProcessListItemComponent } from './click-start-data-process-list-item.component';
+import { ClickStartDataProcessListItemComponent }
+	from './click-start-data-process-list-item.component';
 import { TinyLoadingSpinnerModule }
 	from '../tiny-loading-spinner/tiny-loading-spinner.module';
 
 
 @NgModule({
 	declarations: [ClickStartDataProcessListItemComponent],
-	imports: [
-		CommonModule, TinyLoadingSpinnerModule
-	],
+	imports: [CommonModule, TinyLoadingSpinnerModule],
 	exports: [ClickStartDataProcessListItemComponent]
 })
-export class StartDataProcessListItemModule {
+export class ClickStartDataProcessListItemModule {
 }
