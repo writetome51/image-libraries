a226e487827d8c55f25d6e77876553844c5ae6ad
Steve Thompson <writetome51@gmail.com>
2020-09-30 16:35:10 -0600

    Deleted SelectedImagesDeleterService

--- src/app/shared/services/delete-selected-images-processor/delete-selected-images-processor.service.ts
+++ src/app/shared/services/delete-selected-images-processor/delete-selected-images-processor.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
 import { DataTransportProcessorService }
 	from '@data-transport-processor/data-transport-processor.service';
-import { SelectedImagesDeleterService } from './selected-images-deleter.service';
 import { DeleteSelectedImagesResultInterpreterService }
 	from './delete-selected-images-result-interpreter/delete-selected-images-result-interpreter.service';
+import { GetObjectFromSubscriptionService as getObjectFromSubscription }
+	from '@services/get-object-from-subscription.service';
+import { ImagesRestAPIService } from '@services/images-rest-api.service';
+import { Injectable } from '@angular/core';
+import { LocalSessionIDService } from '@services/local-storage-data/local-session-id.service';
+import { SelectedImageNamesData as selectedImageNames }
+	from '@runtime-state-data/selected-image-names.data';
 
 
 @Injectable({providedIn: 'root'})
@@ -11,7 +16,8 @@ import { DeleteSelectedImagesResultInterpreterService }
 export class DeleteSelectedImagesProcessorService extends DataTransportProcessorService {
 
 	constructor(
-		private __selectedImagesDeleter: SelectedImagesDeleterService,
+		private __imagesRestApi: ImagesRestAPIService,
+		private __localSessionID: LocalSessionIDService,
 		__deleteSelectedImagesResultInterpreter: DeleteSelectedImagesResultInterpreterService
 	) {
 		super(__deleteSelectedImagesResultInterpreter);
@@ -19,7 +25,11 @@ export class DeleteSelectedImagesProcessorService extends DataTransportProcessor
 
 
 	protected async _getResult(): Promise<{ success: true } | { error: { message: string } }> {
-		return await this.__selectedImagesDeleter.delete();
+		return await getObjectFromSubscription.go(
+			this.__imagesRestApi.delete(
+				{sessionID: this.__localSessionID.get(), imageNames: selectedImageNames.data}
+			)
+		);
 	}
 
 }
