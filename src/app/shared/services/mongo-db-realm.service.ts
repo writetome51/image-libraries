81b7d38351f67eca35c2f101b32f606ca2ed5dc1
Steve Thompson <writetome51@gmail.com>
2020-12-18 01:07:47 -0700

    Minor change to MongoDBRealmService

--- src/app/shared/services/mongo-db-realm.service.ts
+++ src/app/shared/services/mongo-db-realm.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import * as Realm from 'realm-web';
 import { pswd, email } from '@app/.special/.realm-eml-pswrd';
 
+
 // Connects with cloud service MongoDB Realm and allows us to call serverless functions we've
 // deployed on it.
 
 @Injectable({providedIn: 'root'})
 export class MongoDBRealmService {
 
-	private __app = new Realm.App({id: 'serverless-functions-rhfqi'});
 	private __user: Realm.User;
 
 
 	constructor() {
-		if (!(this.__user)) {
-			this.__app.logIn(
-				Realm.Credentials.emailPassword(email, pswd)
-			).then((user: Realm.User) => this.__user = user);
-		}
+		let app = new Realm.App({id: 'serverless-functions-rhfqi'});
+
+		app.logIn(
+			Realm.Credentials.emailPassword(email, pswd)
+		).then((user: Realm.User) => this.__user = user);
 	}
 
 
