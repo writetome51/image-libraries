bd934506cf6de16616f3327ad1a07f859b7343e9
Steve Thompson <writetome51@gmail.com>
2020-12-23 17:18:54 -0700

    Improved MongoDBRealmService reliability

--- src/app/shared/services/mongo-db-realm.service.ts
+++ src/app/shared/services/mongo-db-realm.service.ts
@@ -1,6 +1,6 @@
+import { email, pswd } from '@app/.special/.realm-eml-pswrd';
 import { Injectable } from '@angular/core';
 import * as Realm from 'realm-web';
-import { pswd, email } from '@app/.special/.realm-eml-pswrd';
 
 
 // Connects with cloud service MongoDB Realm and allows us to call serverless functions we've
@@ -9,15 +9,18 @@ import { pswd, email } from '@app/.special/.realm-eml-pswrd';
 @Injectable({providedIn: 'root'})
 export class MongoDBRealmService {
 
-	private __user: Realm.User;
+	private ____user; // super-private
+	private __realmAppID =  'serverless-functions-rhfqi';
 
+	private get __user(): Realm.User {
+		if (!(this.____user)) {
+			let app = new Realm.App({id: this.__realmAppID});
 
-	constructor() {
-		let app = new Realm.App({id: 'serverless-functions-rhfqi'});
-
-		app.logIn(
-			Realm.Credentials.emailPassword(email, pswd)
-		).then((user: Realm.User) => this.__user = user);
+			app.logIn(
+				Realm.Credentials.emailPassword(email, pswd)
+			).then((user: Realm.User) => this.____user = user);
+		}
+		return this.____user;
 	}
 
 
