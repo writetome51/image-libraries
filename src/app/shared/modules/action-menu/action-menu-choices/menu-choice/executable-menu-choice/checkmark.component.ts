981ceaad651f31d8fe5f57622982abc7a3388bc1
Steve Thompson <writetome51@gmail.com>
2021-01-20 17:11:19 -0700

    CheckmarkComponent now uses a tiny png image instead of a bootstrap glyphicon

--- src/app/shared/modules/action-menu/action-menu-choices/menu-choice/executable-menu-choice/checkmark.component.ts
+++ src/app/shared/modules/action-menu/action-menu-choices/menu-choice/executable-menu-choice/checkmark.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 
 @Component({
 	selector: 'checkmark',
-
-	// Uses a bootstrap glyphicon
-	template: `<span class="glyphicon glyphicon-ok"></span>`
+	template: `
+		<img [src]="checkmark.src" [width]="checkmark.width" [height]="checkmark.height"
+			 [alt]="checkmark.alt"
+		/>
+	`
 })
 export class CheckmarkComponent {
+
+	checkmark = {
+		src: environment.assets + 'checkmark.png',
+		width: 20,
+		height: 16,
+		alt: 'checkmark'
+	};
+
 }
