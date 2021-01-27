825d5607d1c2cd3da06694d4705f24293bca2367
Steve Thompson <writetome51@gmail.com>
2021-01-27 12:28:03 -0700

    Created service assure-user-images-total-is-stored-locally.service.ts to be used immediately after the user logs in.  The intent is to store the total num of users images in browser so that number doesnt have to be repeatedly retrieved from the database

---
+++ src/app/shared/services/assure-user-images-total-is-stored-locally.service.ts
@@ -0,0 +1,38 @@
+import { BackgroundExecutionStatusData as executionStatus }
+	from '@runtime-state-data/background-execution-status.data';
+import { ExecuteFunctionRequiringWaitingService as executeFunctionRequiringWaiting }
+	from '@services/execute-function-requiring-waiting.service';
+import { hasValue } from '@writetome51/has-value-no-value';
+import { IDoThis } from '@interfaces/i-do-this.interface';
+import { Injectable } from '@angular/core';
+import { UserImagesTotalInBrowserStorageService }
+	from '@item-in-browser-storage/user-images-total-in-browser-storage.service';
+import { GetUserImageTotalProcessorService }
+	from '@get-images-total-processor/get-user-image-total-processor.service';
+
+
+@Injectable({providedIn: 'root'})
+export class AssureUserImagesTotalIsStoredLocallyService implements IDoThis {
+
+	constructor(
+		private __userImagesTotalInBrowser: UserImagesTotalInBrowserStorageService,
+		private __getUserImagesTotalProcessor: GetUserImageTotalProcessorService
+	) {
+	}
+
+
+	async go() {
+		if (this.__userImagesTotalIsStoredLocally()) return;
+
+		else await executeFunctionRequiringWaiting.go(
+			() => this.__getUserImagesTotalProcessor.process(), executionStatus
+		);
+	}
+
+
+	private __userImagesTotalIsStoredLocally(): boolean {
+		let total = this.__userImagesTotalInBrowser.get();
+		return hasValue(total);
+	}
+
+}
