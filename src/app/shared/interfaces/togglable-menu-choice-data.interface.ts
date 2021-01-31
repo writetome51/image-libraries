da78d59a07421f5e1a64aec0b3558ebded8790d8
Steve Thompson <writetome51@gmail.com>
2021-01-30 19:11:37 -0700

    Changed name of interface to TogglableMenuChoiceData

---
+++ src/app/shared/interfaces/togglable-menu-choice-data.interface.ts
@@ -0,0 +1,10 @@
+import { CheckableMenuChoiceData } // tslint:disable-next-line:max-line-length
+	from '@action-menu/action-menu-choices/menu-choice/executable-menu-choice/checkable-menu-choice-data.interface';
+import { DataContainer } from '@interfaces/data-container.interface';
+
+
+export interface TogglableMenuChoiceData extends CheckableMenuChoiceData {
+
+	toggleSetting: DataContainer<{ enabled: boolean }>;
+
+}
