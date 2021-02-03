3d525a08a1d9ad587dafb08c8cb50896c7402223
Steve Thompson <writetome51@gmail.com>
2021-02-03 14:33:26 -0700

    minor changes

--- src/app/update-user/update-email-form/update-email-form.module.ts
+++ src/app/update-user/update-email-form/update-email-form.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UpdateEmailFormComponent } from './update-email-form.component';
 import { UpdateEmailFormInputsComponent } from './update-email-form-inputs.component';
+import { SubmitFormButtonModule } from '@submit-form-button/submit-form-button.module';
+import { ClearFormOnInitModule } from '@clear-form-on-init/clear-form-on-init.module';
+import { ClearAlertOnDestroyModule } from '@clear-alert-on-destroy/clear-alert-on-destroy.module';
+import { AppValidatingFormInputsModule } from '@app-validating-form-inputs/app-validating-form-inputs.module';
 
 
 @NgModule({
@@ -11,6 +15,10 @@ import { UpdateEmailFormInputsComponent } from './update-email-form-inputs.compo
 	],
 	imports: [
 		CommonModule,
+		AppValidatingFormInputsModule,
+		SubmitFormButtonModule,
+		ClearFormOnInitModule,
+		ClearAlertOnDestroyModule
 	],
 	exports: [UpdateEmailFormComponent]
 })
--- src/app/update-user/update-password-form/update-password-form.module.ts
+++ src/app/update-user/update-password-form/update-password-form.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UpdatePasswordFormComponent } from './update-password-form.component';
 import { UpdatePasswordFormInputsComponent } from './update-password-form-inputs.component';
+import { SubmitFormButtonModule } from '@submit-form-button/submit-form-button.module';
+import { ClearFormOnInitModule } from '@clear-form-on-init/clear-form-on-init.module';
+import { ClearAlertOnDestroyModule } from '@clear-alert-on-destroy/clear-alert-on-destroy.module';
+import { AppValidatingFormInputsModule }
+	from '@app-validating-form-inputs/app-validating-form-inputs.module';
 
 
 @NgModule({
@@ -11,6 +16,10 @@ import { UpdatePasswordFormInputsComponent } from './update-password-form-inputs
 	],
 	imports: [
 		CommonModule,
+		SubmitFormButtonModule,
+		ClearFormOnInitModule,
+		ClearAlertOnDestroyModule,
+		AppValidatingFormInputsModule
 	],
 	exports: [UpdatePasswordFormComponent]
 })
