f2493dd3c0b87969b10a3354a5e217c7934892df
Steve Thompson <writetome51@gmail.com>
2021-02-05 15:46:30 -0700

    Added comments for clarification

--- src/app/shared/modules/action-menu/action-menu-choices/menu-choice/remove-parent-labels-for-display.service.ts
+++ src/app/shared/modules/action-menu/action-menu-choices/menu-choice/remove-parent-labels-for-display.service.ts
@@ -4,11 +4,12 @@ import { getByIndex } from '@writetome51/array-get-by-index';
 
 export class RemoveParentLabelsForDisplayService { // implements IDoThis
 
-	static go(label: string) {
-		// If this menu choice is in a submenu, its label will have dot-notation.
-		// Only the text after the final dot should be visible.
+	static go(menuChoiceLabel: string) {
+		// If this menu choice is in a submenu, its label will have dot-notation to separate
+		// parent labels from child labels.
+		// All the text before the final dot are parent labels.
 
-		return getByIndex(-1, label.split('.'));
+		return getByIndex(-1, menuChoiceLabel.split('.'));
 	}
 
 }
