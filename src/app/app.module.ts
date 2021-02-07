213db45dcc2fb3e9bef6a6e9a7cfd9cf623c2b0b
Steve Thompson <writetome51@gmail.com>
2021-02-07 14:44:23 -0700

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { AppAlertComponent } from './app-alert/app-alert.component';
 import { AppComponent } from './app.component';
-import { AppNavigatorModule } from '@app-navigator/app-navigator.module';
+import { LoggedInAppNavigatorModule }
+	from '@logged-in-app-navigator/logged-in-app-navigator.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HTMLImageModule } from '@app/shared/modules/html-image/html-image.module';
+import { HTMLImageModule } from '@html-image/html-image.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
@@ -21,7 +22,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
 		FormsModule,
 		HttpClientModule,
 		HTMLImageModule,
-		AppNavigatorModule,
+		LoggedInAppNavigatorModule,
 		AppRoutingModule
 	],
 	providers: [Title], // for setting <title>
