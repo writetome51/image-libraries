9beac13586e31a1c1667cc0aafc6dddd825d932c
Steve Thompson <writetome51@gmail.com>
2021-02-08 22:01:30 -0700

    abstract method _getResult isnt used anymore

--- src/app/shared/services/processor/processor.service.ts
+++ src/app/shared/services/processor/processor.service.ts
@@ -1,19 +1,26 @@
+import { HasError } from '@interfaces/has-error.interface';
 import { Processor } from '@interfaces/processor.interface';
 import { ProcessResultInterpreterService } from './process-result-interpreter.service';
+import { IDoThis } from '@interfaces/i-do-this.interface';
 
 
 export abstract class ProcessorService implements Processor {
 
-	constructor(private __resultInterpreter: ProcessResultInterpreterService) {
+	constructor(
+		private __getResult: IDoThis,
+		private __resultInterpreter: ProcessResultInterpreterService
+	) {
 	}
 
 
 	async process(...args): Promise<void> {
-		let result = await this._getResult(...args);
+		let result = await this.__getResult.go(...args);
 		await this.__resultInterpreter.interpret(result);
 	}
 
 
-	protected abstract _getResult(...args): any | { error: { message: string } };
+	protected async _getResult(...args): Promise<any | HasError> {
+		return await this.__getResult.go(...args);
+	}
 
 }
