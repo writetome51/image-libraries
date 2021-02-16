5a987e0b0084712758ed4b339c8f4f1401a853a5
Steve Thompson <writetome51@gmail.com>
2021-02-16 11:44:17 -0700

    Added comments

--- src/app/shared/services/execute-function-requiring-waiting.service.ts
+++ src/app/shared/services/execute-function-requiring-waiting.service.ts
@@ -1,6 +1,9 @@
 // import { IDoThis } from '@interfaces/i-do-this.interface';
 
 
+// To help let user know the app is working on their request.
+// While `executionStatus.waiting` === true, you can show 'loading...' message
+
 export class ExecuteFunctionRequiringWaitingService { // implements IDoThis
 
 	static async go(
