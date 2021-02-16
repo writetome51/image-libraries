9c04e096da65773881d394d16b2e1a131b3183d2
Steve Thompson <writetome51@gmail.com>
2021-02-16 11:36:09 -0700

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { AppAlertComponent } from './app-alert/app-alert.component';
 import { AppComponent } from './app.component';
-import { LoggedInNavigatorModule }
-	from '@app/logged-in-navigator/logged-in-navigator.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HTMLImageModule } from '@html-image/html-image.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LoggedInNavigatorModule } from '@app/logged-in-navigator/logged-in-navigator.module';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found.component';
 
@@ -25,7 +24,7 @@ import { PageNotFoundComponent } from './page-not-found.component';
 		LoggedInNavigatorModule,
 		AppRoutingModule
 	],
-	providers: [Title], // for setting <title>
+	providers: [Title], // for setting html <title>
 
 	bootstrap: [AppComponent]
 })
