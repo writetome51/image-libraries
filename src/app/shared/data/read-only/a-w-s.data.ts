5577f3cd632e018941648205036f2f7fecbe5514
Steve Thompson <writetome51@gmail.com>
2021-04-06 13:47:58 -0600

    Still integrating AWS S3 SDK into this app

---
+++ src/app/shared/data/read-only/a-w-s.data.ts
@@ -0,0 +1,7 @@
+export class AWSData {
+
+	static readonly region = 'us-east-1';
+	static readonly identityPoolId = 'us-east-1:b3cf6fc7-081d-4b98-8761-c37d78744742';
+	static readonly s3Bucket = 'image-libraries-app';
+
+}
