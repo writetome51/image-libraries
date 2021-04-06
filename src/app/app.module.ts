0bfb0ae7fd892832d0b3d4013eac42bc9222f971
Steve Thompson <writetome51@gmail.com>
2021-04-06 17:14:39 -0600

    Removed Cloudinary module from project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { AppAlertComponent } from './app-alert.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { CloudinaryModule } from '@cloudinary/angular-5.x';
-import * as  Cloudinary from 'cloudinary-core';
 import { FormsModule } from '@angular/forms';
 import { HTMLImageModule } from '@html-image_module/html-image.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -24,7 +22,6 @@ import { PageNotFoundComponent } from './page-not-found.component';
 		HTMLImageModule,
 		HttpClientModule,
 		LoggedInNavigatorModule,
-		CloudinaryModule.forRoot(Cloudinary, { cloud_name: 'dnpg3umhb',  upload_preset: 'tiqeiglj'}),
 		AppRoutingModule
 	],
 	providers: [Title], // for setting html <title>.  Available app-wide.
