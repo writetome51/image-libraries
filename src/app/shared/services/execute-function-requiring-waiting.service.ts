67982342b8ef58404a81ee520ec2e2b5260ed738
Steve Thompson <writetome51@gmail.com>
2021-04-27 15:23:40 -0600

    Minor changes from previous commit

--- src/app/shared/services/execute-function-requiring-waiting.service.ts
+++ src/app/shared/services/execute-function-requiring-waiting.service.ts
@@ -3,18 +3,18 @@ import { Waiter } from '@interfaces/waiter.interface';
 
 
 // To help let user know the app is working on their request.
-// While `executionStatus.waiting` === true, you can show 'loading...' message
+// While `executionStatus.waiting === true`, you can show 'loading...' message
 
 export class ExecuteFunctionRequiringWaitingService { // implements IDoThis
 
-	static async go(
-		func: (...args) => any,
+	static async go<T>(
+		func: (...args) => T,
 		executionStatus: Waiter,
 		funcArgs = []
-	): Promise<any> {
+	): Promise<T> {
 
 		executionStatus.waiting = true;
-		let result: any = await func(...funcArgs);
+		let result: T = await func(...funcArgs);
 		executionStatus.waiting = false;
 
 		return result;
--- src/app/shared/services/execute-loop-requiring-progress-update.service.ts
+++ src/app/shared/services/execute-loop-requiring-progress-update.service.ts
@@ -2,15 +2,18 @@ import { getRoundedDown } from '@writetome51/get-rounded-up-down';
 // import { IDoThis } from '@interfaces/i-do-this.interface';
 
 
+// To help implement an animated progress bar
+
 export class ExecuteLoopRequiringProgressUpdateService { // implements IDoThis
 
 	static async go<T>(
 		arr: T[],
-		func: (value, index?, arr?: T[]) => void,
+		forEach: (value: T, index?: number, arr?: T[]) => void,
 		progress: { percentageComplete: number },
 	): Promise<void> {
+
 		for (let i = 0, length = arr.length; i < length; ++i) {
-			await func(arr[i], i, arr);
+			await forEach(arr[i], i, arr);
 			progress.percentageComplete = getRoundedDown(((i + 1) / length) * 100);
 		}
 	};
