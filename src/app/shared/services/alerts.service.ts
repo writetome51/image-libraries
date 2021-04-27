2e2aaf338ff528618816ac23d448e3f97d324e34
Steve Thompson <writetome51@gmail.com>
2021-04-27 16:36:55 -0600

    AlertService is now called AlertsService. It now can contain multiple alerts at once.

---
+++ src/app/shared/services/alerts.service.ts
@@ -0,0 +1,34 @@
+import { removeByIndex } from '@writetome51/array-remove-by-index';
+import { Alert } from '@interfaces/alert.interface';
+
+
+export class AlertsService {
+
+	private static __data: Alert[] = [];
+
+
+	static getAlert(index: number): Alert {
+		return this.__data[index];
+	}
+
+
+	static setSuccess(message) {
+		this.__data.push({message, isError: false});
+	}
+
+
+	static setError(message) {
+		this.__data.push({message, isError: true});
+	}
+
+
+	static clearAlert(index: number) {
+		removeByIndex(index, this.__data);
+	}
+
+
+	static clearAll() {
+		this.__data = [];
+	}
+
+}
