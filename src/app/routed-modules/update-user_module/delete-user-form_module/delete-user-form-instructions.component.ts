59db92599cfdd841c8c673c22c3f2c56b9640b3a
Steve Thompson <writetome51@gmail.com>
2021-05-02 11:14:38 -0600

    Added DeleteUserFormInstructionsComponent

---
+++ src/app/routed-modules/update-user_module/delete-user-form_module/delete-user-form-instructions.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HTMLImage } from '@interfaces/html-image.interface';
+import { environment } from '@environments/environment';
+
+
+@Component({
+	selector: 'delete-user-form-instructions',
+	template: `
+		<div>
+			<html-image [data]="explodingHead"></html-image>
+			<p class="warning-text">You cannot undo this.</p>
+
+			<p>If you wish to proceed, enter your info and click Delete</p>
+		</div>
+	`,
+	styles: ['.warning-text { color: red; font-weight: bold; }']
+})
+export class DeleteUserFormInstructionsComponent {
+
+	explodingHead: HTMLImage = {
+		src: environment.assets + 'head-exploding.gif',
+		alt: 'Exploding head',
+		width: 124
+	};
+
+}
