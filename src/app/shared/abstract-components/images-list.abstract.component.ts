292bddecd4928252070706aeb79d445382cb874b
Steve Thompson <writetome51@gmail.com>
2021-05-15 14:16:08 -0600

    Created abstract component ImagesViewerContainerComponent. ImagesListComponent no longer has a paginator injected into it

--- src/app/shared/abstract-components/images-list.abstract.component.ts
+++ src/app/shared/abstract-components/images-list.abstract.component.ts
@@ -1,15 +1,10 @@
 import { ImageRecord } from '@interfaces/image-record.interface';
-import { GetPageImagesService as getPageImages } from '@services/get-page-images.service';
-import { AppPaginatorService } from '@app-paginator/app-paginator.abstract.service';
+import { Component, Input } from '@angular/core';
 
 
+@Component({selector: '', template: ''}) // prevents Angular build error
 export abstract class ImagesListComponent {
 
-	get images(): ImageRecord[] {
-		return getPageImages.go(this.__paginator);
-	}
-
-
-	constructor(private __paginator: AppPaginatorService) {}
+	@Input() data: ImageRecord[];
 
 }
---
+++ src/app/shared/abstract-components/images-viewer-container.abstract.component.ts
@@ -0,0 +1,27 @@
+import { Gettable } from '@interfaces/gettable.interface';
+import { AppPaginatorService } from '@app-paginator/app-paginator.abstract.service';
+import { JumpToPageInputService }
+	from '@app-pagination-controls_module/jump-to-page-input.abstract.service';
+import { ImageRecord } from '@interfaces/image-record.interface';
+import { GetPageImagesService as getPageImages } from '@services/get-page-images.service';
+
+
+export abstract class ImagesViewerContainerComponent {
+
+	abstract routeBeforePageNumber: string;
+	abstract paginator: AppPaginatorService;
+	abstract jumpToPageInput: JumpToPageInputService;
+
+
+	get imageTotal(): number {
+		return this.__storedImageTotal.get();
+	}
+
+	get images(): ImageRecord[] {
+		return getPageImages.go(this.paginator);
+	}
+
+
+	constructor(private __storedImageTotal: Gettable<number>) {}
+
+}
