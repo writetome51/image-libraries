120d6c49267a520faf587cf4117244ec787da916
Steve Thompson <writetome51@gmail.com>
2021-05-14 20:30:41 -0600

    SubmitFormButtonComponent now receives ValidatingInputs as an @Input.

--- src/app/shared/modules/submit-form-button_module/submit-form-button.component.ts
+++ src/app/shared/modules/submit-form-button_module/submit-form-button.component.ts
@@ -3,7 +3,8 @@ import { Component, Input } from '@angular/core';
 import { ClickExecuteIDoThisContainerComponent }
 	from '@abstract-components/click-execute-i-do-this-container.abstract.component';
 import { SendFormData } from '@interfaces/send-form-data.interface';
-
+import { ValidatingInputService, ValidatingInputsService } from '@writetome51/validating-inputs';
+import { isArray } from '@writetome51/is-array-not-array';
 
 @Component({
 	selector: 'submit-form-button',
@@ -16,18 +17,24 @@ import { SendFormData } from '@interfaces/send-form-data.interface';
 export class SubmitFormButtonComponent extends ClickExecuteIDoThisContainerComponent {
 
 	@Input() label = 'Submit';
+	@Input() validatingInputs: ValidatingInputService | ValidatingInputsService;
+
 
 	@Input() set iDoThis(sendFormData: SendFormData) {
 		const sendFormData_go = sendFormData.go;
-		this._iDoThis = sendFormData;
 
-		this._iDoThis.go = async (...args) => {
-			if (sendFormData.$_validatingInputs.isValid()) {
-				await sendFormData_go.apply(sendFormData, args);
+		this._iDoThis = {
+			go: async () => {
+				if (this.validatingInputs.isValid()) {
+					let inputValues = this.validatingInputs.getValue();
+					await sendFormData_go.apply(
+						sendFormData,
+						isArray(inputValues) ? inputValues : [inputValues]
+					);
+				}
+				else alerts.setError(this.validatingInputs.error);
 			}
-			else alerts.setError(sendFormData.$_validatingInputs.error);
 		};
-
 	}
 
 
