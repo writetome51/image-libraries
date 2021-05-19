3b8ae45516d4b9e640d5598aa6e4a4d9b23d1274
Steve Thompson <writetome51@gmail.com>
2021-05-19 15:16:35 -0600

    Moved AppAlertsModule inside AppMessageCenterModule

--- src/app/shared/modules/app-message-center_module/app-alerts_module/app-alerts.module.ts
+++ src/app/shared/modules/app-message-center_module/app-alerts_module/app-alerts.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppAlertComponent } from '@app/app-alerts_module/app-alert.component';
-import { AppAlertsComponent } from '@app/app-alerts_module/app-alerts.component';
+import { AppAlertComponent } from './app-alert.component';
+import { AppAlertsComponent } from './app-alerts.component';
 
 
 @NgModule({
