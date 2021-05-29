299630083778e168cd5a52896e79651169ebe766
Steve Thompson <writetome51@gmail.com>
2021-05-29 00:27:45 -0600

    Deleted unnecessary files

--- src/app/shared/abstract-components/has-context-input.abstract.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-
-@Component({selector: '', template: ''}) // prevents Angular build error
-export abstract class HasContextInputComponent<T> {
-
-	@Input() context: T;
-
-}
--- src/app/shared/abstract-components/has-data-input.abstract.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-
-@Component({selector: '', template: ''}) // prevents Angular build error
-export abstract class HasDataInputComponent<T> {
-
-	@Input() data: T;
-
-}
