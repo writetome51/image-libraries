78df7b9dc19654220c00a9c1838f52aeaf3ba2d4
Steve Thompson <writetome51@gmail.com>
2021-06-07 12:52:20 -0600

    Created AppFooterComponent

---
+++ src/app/app-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+	selector: 'app-footer',
+	template: `
+		<footer id="site-footer">
+			<p>
+				This site does not use SSL. Do not pass sensitive information.
+				For demonstration purposes only. Not intended for commercial use.
+			</p>
+		</footer>
+	`
+})
+export class AppFooterComponent {}
