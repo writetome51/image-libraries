5811bd5345619830752fbd963df3eaff08bcfccd
Steve Thompson <writetome51@gmail.com>
2021-06-11 14:08:33 -0600

    GetRouteParamsObserverService no longer manipulates BackgroundExecutionStatusData while it runs its tasks

--- src/app/shared/services/get-route-params-observer.abstract.service.ts
+++ src/app/shared/services/get-route-params-observer.abstract.service.ts
@@ -1,7 +1,3 @@
-import { BackgroundExecutionStatusData as executionStatus }
-	from '@runtime-state-data/background-execution-status.data';
-import { ExecuteFunctionRequiringWaitingService as executeFunctionRequiringWaiting }
-	from '@services/execute-function-requiring-waiting.service';
 import { IDoThis } from '@interfaces/i-do-this.interface';
 
 
@@ -11,13 +7,7 @@ export abstract class GetRouteParamsObserverService implements IDoThis {
 
 
 	go(): (params) => Promise<void> {
-
-		return async (params) => {
-			await executeFunctionRequiringWaiting.go(
-				() => this.__runTasksAfterRouteParamsReceived.go(params),
-				executionStatus
-			);
-		};
+		return async (params) => this.__runTasksAfterRouteParamsReceived.go(params);
 	}
 
 }
