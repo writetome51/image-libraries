2f5ba94454ea5f0e738166a8f9c9353e83c227e7
Steve Thompson <writetome51@gmail.com>
2021-06-12 10:24:16 -0600

    Minor changes

--- src/app/routed-modules/add-images_module/process-save-new-image-records_service/interpret-save-new-image-records-result_service/run-tasks-after-saving-new-image-records.service.ts
+++ src/app/routed-modules/add-images_module/process-save-new-image-records_service/interpret-save-new-image-records-result_service/run-tasks-after-saving-new-image-records.service.ts
@@ -4,31 +4,32 @@ import { NewImagesData as newImages }
 	from '@runtime-state-data/static-classes/auto-resettable.data';
 import { IDoThis } from '@interfaces/i-do-this.interface';
 import { AddImagesServicesModule } from '../../add-images-services.module';
-import { RunTasksAfterModifyingLoadedImagesService }
-	from '@run-post-success-tasks/run-tasks-after-modifying-loaded-images.service';
 import { UserImageTotalInBrowserStorageService }
 	from '@browser-storage/user-image-total-in-browser-storage.service';
 import { newImagesSaved } from '@string-constants/alert-success-messages';
+import { LoadedImagesStateService }
+	from '@services/loaded-image-state_service/loaded-images-state.service';
 
 
 @Injectable({providedIn: AddImagesServicesModule})
 export class RunTasksAfterSavingNewImageRecordsService implements IDoThis {
 
 	constructor(
-		private __runTasksAfterModifyingLoadedImages: RunTasksAfterModifyingLoadedImagesService,
-		private __userImageTotal: UserImageTotalInBrowserStorageService
+		private __userImageTotal: UserImageTotalInBrowserStorageService,
+		private __loadedImagesState: LoadedImagesStateService
 	) {}
 
+
 	go() {
-		alerts.setSuccess(newImagesSaved);
 		this.__emptyImageDataSoItWillBeForcedToRefresh();
+		alerts.setSuccess(newImagesSaved);
 	}
 
 
 	private __emptyImageDataSoItWillBeForcedToRefresh() {
 		newImages.setDefault();
 		this.__userImageTotal.remove();
-		this.__runTasksAfterModifyingLoadedImages.go();
+		this.__loadedImagesState.setDefault();
 	}
 
 }
