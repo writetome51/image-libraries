18b420f2711e3baef7f3696c519deb8e9323133a
Steve Thompson <writetome51@gmail.com>
2021-06-24 13:42:48 -0600

    In interface ActionMenuContext, property "getChoicesArgs" is now required

--- src/app/shared/services/library/if-updated-library-is-being-viewed-then-reload-current-page-data.service.ts
+++ src/app/shared/services/library/if-updated-library-is-being-viewed-then-reload-current-page-data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { IDoThis } from '@interfaces/i-do-this.interface';
-import { LoadedImagesStateService } from '@services/loaded-images-state_service/loaded-images-state.service';
-import { ReloadCurrentPageDataService } from '@services/reload-current-page-data_service/reload-current-page-data.service';
-import { LoadedLibraryInBrowserStorageService } from '@browser-storage/loaded-library-in-browser-storage.service';
+import { LoadedImagesStateService }
+	from '@services/loaded-images-state_service/loaded-images-state.service';
+import { LoadedLibraryInBrowserStorageService }
+	from '@browser-storage/loaded-library-in-browser-storage.service';
 import { noValue } from '@writetome51/has-value-no-value';
+import { ReloadCurrentPageDataService }
+	from '@services/reload-current-page-data_service/reload-current-page-data.service';
 
 
 @Injectable({providedIn: 'root'})
