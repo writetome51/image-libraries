4a6528591713889c2b01c363c54311fe8d672b53
Steve Thompson <writetome51@gmail.com>
2021-07-06 13:48:58 -0600

    Minor changes

--- src/app/routed-modules/libraries_module/libraries-viewer_module/search-library-names_component/search-libraries.service.ts
+++ src/app/routed-modules/libraries_module/libraries-viewer_module/search-library-names_component/search-libraries.service.ts
@@ -1,14 +1,13 @@
+import { getByTest } from '@writetome51/array-get-by-test';
 import { Injectable } from '@angular/core';
-import { SearchData as search } from '@runtime-state-data/search.data';
+import { IDoThis } from '@interfaces/i-do-this.interface';
 import { LibrariesInBrowserStorageService }
 	from '@browser-storage/libraries-in-browser-storage.service';
-import { getByTest } from '@writetome51/array-get-by-test';
-import { IDoThis } from '@interfaces/i-do-this.interface';
 import { LibrariesViewerServicesModule } from '../libraries-viewer-services.module';
-import { SearchedLibraryNamesData as searchedLibraryNames } from '../searched-library-names.data';
-import { isEmpty } from '@writetome51/is-empty-not-empty';
 import { LibraryNamesData as libraryNames }
 	from '@runtime-state-data/static-classes/auto-resettable.data';
+import { SearchData as search } from '@runtime-state-data/search.data';
+import { SearchedLibraryNamesData as searchedLibraryNames } from '../searched-library-names.data';
 
 
 @Injectable({providedIn: LibrariesViewerServicesModule})
