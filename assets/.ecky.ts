86ef5a310ed942a203893cf26c62535420232479
Steve Thompson <writetome51@gmail.com>
2021-07-12 12:42:30 -0600

    Auto-generated commit

--- assets/.ecky.ts
+++
@@ -1,1 +0,0 @@
-export const ecky = '5e8a19bDz7d053b89ffa697v43b62c1d053b89fj053b89Aw'; // 48 chars.
--- assets/.sss.ts
+++
@@ -1,4 +0,0 @@
-// In mongodb stitch, this is a secret called 'super-secret'.
-// If you change the value here, you must change its value in mongodb stitch to match.
-
-export const sss = 'iD14356703211-*@qtfaxzSRQBPUKTNJ';
