ad9c5d7b01c0e29543ed8bc0b2079d85b12b5094
Steve Thompson <writetome51@gmail.com>
2021-07-13 15:44:11 -0600

    Make SetCurrentPageImagesService update paginator's configuration before setting current page

--- src/app/shared/services/set-current-page-images.service.ts
+++ src/app/shared/services/set-current-page-images.service.ts
@@ -1,13 +1,22 @@
+import { ConfigurePaginatorService as configurePaginator }
+	from '@app-paginator/configure-paginator.service';
 import { CurrentPageImagesData as currentPageImages }
 	from '@runtime-state-data/static-classes/auto-resettable.data';
 import { ImageRecord } from '@interfaces/image-record.interface';
-import { setArray } from '@writetome51/set-array';
 import { IDoThis } from '@interfaces/i-do-this.interface';
+import { Injectable } from '@angular/core';
+import { PageSizeInBrowserStorageService }
+	from '@browser-storage/page-size-in-browser-storage.service';
+import { setArray } from '@writetome51/set-array';
 
 
-export class __SetCurrentPageImagesService {
+@Injectable({providedIn: 'root'})
+export class SetCurrentPageImagesService implements IDoThis {
 
-	static async go(
+	constructor(private __pageSizeInBrowser: PageSizeInBrowserStorageService) {}
+
+
+	async go(
 		pageNum: number,
 		paginator: {
 			setCurrentPageNumber: (num: number, option?: { reload: boolean }) => Promise<void>,
@@ -16,18 +25,12 @@ export class __SetCurrentPageImagesService {
 		option = {reload: false}
 
 	): Promise<void> {
-		try {
-			await paginator.setCurrentPageNumber(pageNum, option);
 
-			// Caches reference to paginator's current page, which is emptied on logout.
-			setArray(currentPageImages.data, paginator.getCurrentPage());
-		}
-			// If the current page doesnt exist it triggers error, so we catch it:
-		catch (e) {
-			console.log(e.message);
-		}
+		configurePaginator.go(paginator, this.__pageSizeInBrowser.get());
+		await paginator.setCurrentPageNumber(pageNum, option);
+
+		// Caches reference to paginator's current page, which is emptied on logout.
+		setArray(currentPageImages.data, paginator.getCurrentPage());
 	}
 
 }
-
-export const SetCurrentPageImagesService: IDoThis = __SetCurrentPageImagesService;
