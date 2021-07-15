e40d91419a64388cb0c9f3e8188a62c0033a4110
Steve Thompson <writetome51@gmail.com>
2021-07-15 16:49:13 -0600

    Create ActionMenuContextDirective

---
+++ src/app/shared/abstract-directives/action-menu-context.abstract.directive.ts
@@ -0,0 +1,23 @@
+import { ActionMenuContext } from '@interfaces/action-menu-context.interface';
+import { ActionMenuContextChoicesExecutorService }
+	from '@services/action-menu-context-choices-executor.abstract.service';
+import { MenuChoicesManager } from '@interfaces/menu-choices-manager.interface';
+import { MenuChoice } from '@interfaces/menu-choice.interface';
+
+
+export abstract class ActionMenuContextDirective implements ActionMenuContext {
+
+	getChoicesArgs: any[];
+
+
+	constructor(
+		public menuChoicesManager: MenuChoicesManager,
+		public choicesExecutor: ActionMenuContextChoicesExecutorService,
+	) {}
+
+
+	getChoices(): MenuChoice[] {
+		return this.menuChoicesManager.getChoices(...this.getChoicesArgs);
+	}
+
+}
