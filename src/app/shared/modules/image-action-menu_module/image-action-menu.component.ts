c6beded0bf69f3e3e559da3cea65ddc0f7f6a0ab
Steve Thompson <writetome51@gmail.com>
2021-07-15 17:03:33 -0600

    ImageActionMenuComponent now extends ActionMenuContextDirective

--- src/app/shared/modules/image-action-menu_module/image-action-menu.component.ts
+++ src/app/shared/modules/image-action-menu_module/image-action-menu.component.ts
@@ -1,25 +1,27 @@
-import { AppImage } from '@interfaces/app-image.interface';
-import { ActionMenuContext } from '@interfaces/action-menu-context.interface';
 import { Component, Input, OnInit } from '@angular/core';
 import { ImageActionMenuChoicesManagerService } from './image-action-menu-choices-manager.service';
 import { ImageActionMenuChoicesExecutorService }
 	from './image-action-menu-choices-executor_service/image-action-menu-choices-executor.service';
+import { ImageRecord } from '@interfaces/image-record.interface';
+import { ActionMenuContextDirective }
+	from '@abstract-directives/action-menu-context.abstract.directive';
 
 
 @Component({
 	selector: 'image-action-menu',
 	template: `<action-menu [context]="this"></action-menu>`
 })
-export class ImageActionMenuComponent implements ActionMenuContext, OnInit {
+export class ImageActionMenuComponent extends ActionMenuContextDirective implements OnInit {
 
-	@Input() image: AppImage;
-	getChoicesArgs = [];
+	@Input() image: ImageRecord;
 
 
 	constructor(
-		public menuChoicesManager: ImageActionMenuChoicesManagerService,
-		public choicesExecutor: ImageActionMenuChoicesExecutorService
-	) {}
+		menuChoicesManager: ImageActionMenuChoicesManagerService,
+		choicesExecutor: ImageActionMenuChoicesExecutorService
+	) {
+		super(menuChoicesManager, choicesExecutor);
+	}
 
 
 	ngOnInit() {
