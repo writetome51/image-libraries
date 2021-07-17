c2487c913316bab58a88f0b76bfbf1fd370bdace
Steve Thompson <writetome51@gmail.com>
2021-07-17 01:15:29 -0600

    Adjust styling for <sprocket-wheel> and span.caret

--- src/app/shared/modules/action-menu_module/action-menu-button_module/sprocket-wheel.component.ts
+++ src/app/shared/modules/action-menu_module/action-menu-button_module/sprocket-wheel.component.ts
@@ -5,13 +5,23 @@ import { HTMLImage } from '@interfaces/html-image.interface';
 
 @Component({
 	selector: 'sprocket-wheel',
-	template: `<html-image [data]="sprocket"></html-image>`
+	template: `
+		<div class="sprocket-wheel-container"
+			 [style.display]="'inline-block'"
+			 [style.position]="'relative'"
+			 [style.width]="image.width + 'px'"
+			 [style.height]="image.height + 'px'"
+			 [style.background-image]="'url(' + image.src + ')'"
+			 [style.background-repeat]="'no-repeat'"
+		></div>
+	`
 })
 export class SprocketWheelComponent {
 
-	readonly sprocket: HTMLImage = {
+	readonly image: HTMLImage = {
 		src: environment.assets + 'sprocket-wheel-big.png',
 		width: 18,
+		height: 18,
 		alt: 'sprocket wheel'
 	};
 
